35fcead834595a97f5dd9b6c1c28cc5a7df122ee
Njordr <njordrenterprises@gmail.com>
2023-12-04 00:09:44 -0600

    rust structs
    structs
    enums
    matches

---
+++ scratch/Rust/target/debug/deps/rust_structs.d
@@ -0,0 +1,5 @@
+C:\Users\njord\Documents\CodeHub\Portfolio_Projects\scratch\Rust\target\debug\deps\rust_structs.exe: src\bin\rust_structs.rs
+
+C:\Users\njord\Documents\CodeHub\Portfolio_Projects\scratch\Rust\target\debug\deps\rust_structs.d: src\bin\rust_structs.rs
+
+src\bin\rust_structs.rs:
---
+++ scratch/Rust/target/debug/rust_structs.d
@@ -0,0 +1,1 @@
+C:\Users\njord\Documents\CodeHub\Portfolio_Projects\scratch\Rust\target\debug\rust_structs.exe: C:\Users\njord\Documents\CodeHub\Portfolio_Projects\scratch\Rust\src\bin\rust_structs.rs
